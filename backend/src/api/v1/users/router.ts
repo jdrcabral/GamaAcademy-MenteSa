f1d9880f36d0f13670dafa4d136d13f8b23da64e
João Denis <joao.denisr@gmail.com>
2022-09-01 20:16:41 -0300

    UPDATE: Swagger

--- backend/src/api/v1/users/router.ts
+++ backend/src/api/v1/users/router.ts
@@ -5,6 +5,13 @@ import { UserController} from './controller';
 const router = Router();
 
 const userRoutes = () => {
+    /**
+     * @swagger
+     * /api/v1/users:
+     *   get:
+     *     summary: Retrieve a list of JSONPlaceholder users
+     *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+    */
     router.get('', UserController.list);
     router.post('', UserController.create);
     router.patch('', UserController.partialUpdate);
